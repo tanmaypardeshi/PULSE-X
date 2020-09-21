6cba3f3f2dee38785039b9640baf211ff8816bac
tanmaypardeshi <tanmaypardeshi@gmail.com>
2020-09-22 00:50:09 +0530

    Added api for employee detail in manager

--- backend/employee/urls.py
+++ backend/employee/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import (ReviewView, SavedReview, GetReview, GetFlagDetails, )
+from .views import (ReviewView, SavedReview, GetReview,
+                    GetFlagDetails, EmployeeDetailView, )
 
 urlpatterns = [
     path('review/', ReviewView.as_view(), name='get-review'),
     path('saved/', SavedReview.as_view(), name='saved'),
     path('review_data/', GetReview.as_view(), name='review'),
-    path('flag_data/', GetFlagDetails.as_view(), name='flag')
+    path('flag_data/', GetFlagDetails.as_view(), name='flag'),
+    path('detail/', EmployeeDetailView.as_view(), name='employee-detail')
 ]
--- backend/employee/views.py
+++ backend/employee/views.py
@@ -12,7 +12,7 @@ from .models import (Review, )
 
 
 class ReviewView(generics.GenericAPIView):
-    permission_classes = (IsAuthenticated, IsEmployee, )
+    permission_classes = (IsAuthenticated, IsEmployee,)
     authentication_classes = (JSONWebTokenAuthentication,)
     serializer_class = ReviewSerializer
 
@@ -48,8 +48,8 @@ class ReviewView(generics.GenericAPIView):
 
 
 class SavedReview(generics.ListAPIView):
-    permission_classes = (IsAuthenticated, IsEmployee, )
-    authentication_classes = (JSONWebTokenAuthentication, )
+    permission_classes = (IsAuthenticated, IsEmployee,)
+    authentication_classes = (JSONWebTokenAuthentication,)
     serializer_class = SaveSerializer
 
     def get_queryset(self):
@@ -78,39 +78,105 @@ class SavedReview(generics.ListAPIView):
                 'message': e.__str__()
             }, status=status.HTTP_400_BAD_REQUEST)
 
+
 class GetReview(generics.ListAPIView):
-    permission_classes = (IsAuthenticated, IsEmployee, )
-    authentication_classes = (JSONWebTokenAuthentication, )
+    permission_classes = (IsAuthenticated, IsEmployee,)
+    authentication_classes = (JSONWebTokenAuthentication,)
 
     def get(self, request, *args, **kwargs):
-        employee_reviews = Review.objects.filter(user=request.user).count()
-        total_reviews = Review.objects.all().count()
+        try:
+            employee_reviews = Review.objects.filter(user=request.user).count()
+            total_reviews = Review.objects.all().count()
 
-        return Response({
-            'employee_reviews': employee_reviews,
-            'difference': total_reviews - employee_reviews,
-            'total_reviews': total_reviews
-        }, status=status.HTTP_200_OK)
+            return Response({
+                'success': True,
+                'message': 'Fetched employee details',
+                'data': {
+                    'employee_reviews': employee_reviews,
+                    'difference': total_reviews - employee_reviews,
+                    'total_reviews': total_reviews
+                }
+            }, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({
+                'success': False,
+                'message': e.__str__()
+            }, status=status.HTTP_400_BAD_REQUEST)
 
 
 class GetFlagDetails(generics.ListAPIView):
-    permission_classes = (IsAuthenticated, IsEmployee, )
-    authentication_classes = (JSONWebTokenAuthentication, )
+    permission_classes = (IsAuthenticated, IsEmployee,)
+    authentication_classes = (JSONWebTokenAuthentication,)
 
     def get(self, request, *args, **kwargs):
-        flag0 = Review.objects.filter(flag=0).count()
-        flag1 = Review.objects.filter(flag=1).count()
-        flag2 = Review.objects.filter(flag=2).count()
-        flag3 = Review.objects.filter(flag=3).count()
-        flag4 = Review.objects.filter(flag=4).count()
-        flag5 = Review.objects.filter(flag=5).count()
-        total = Review.objects.all().count()
-        return Response({
-            'flag0': flag0,
-            'flag1': flag1,
-            'flag2': flag2,
-            'flag3': flag3,
-            'flag4': flag4,
-            'flag5': flag5,
-            'total': total
-        }, status=status.HTTP_200_OK)
+        try:
+            flag0 = Review.objects.filter(flag=0).count()
+            flag1 = Review.objects.filter(flag=1).count()
+            flag2 = Review.objects.filter(flag=2).count()
+            flag3 = Review.objects.filter(flag=3).count()
+            flag4 = Review.objects.filter(flag=4).count()
+            flag5 = Review.objects.filter(flag=5).count()
+            total = Review.objects.all().count()
+            return Response({
+                'success': True,
+                'message': 'Fetched employee details',
+                'data': {
+                    'flag0': flag0,
+                    'flag1': flag1,
+                    'flag2': flag2,
+                    'flag3': flag3,
+                    'flag4': flag4,
+                    'flag5': flag5,
+                    'total': total
+                }
+            }, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({
+                'success': False,
+                'message': e.__str__()
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+
+class EmployeeDetailView(generics.ListAPIView):
+    permission_classes = (IsAuthenticated, IsManager,)
+    authentication_classes = (JSONWebTokenAuthentication,)
+
+    def get(self, request, *args, **kwargs):
+        try:
+            employee = User.objects.get(id=request.data['id'])
+            name = f"{employee.first_name} {employee.last_name}"
+            email = employee.email
+            manager_id = employee.manager_id
+            last_login = employee.last_name
+            date_joined = employee.date_joined
+            flag0 = Review.objects.filter(user=employee, flag=0).count()
+            flag1 = Review.objects.filter(user=employee, flag=1).count()
+            flag2 = Review.objects.filter(user=employee, flag=2).count()
+            flag3 = Review.objects.filter(user=employee, flag=3).count()
+            flag4 = Review.objects.filter(user=employee, flag=4).count()
+            flag5 = Review.objects.filter(user=employee, flag=5).count()
+            total = Review.objects.all().count()
+
+            return Response({
+                'success': True,
+                'message': 'Fetched employee details',
+                'data': {
+                    'name': name,
+                    'email': email,
+                    'manager_id': manager_id,
+                    'last_login': last_login,
+                    'date_joined': date_joined,
+                    'flag0': flag0,
+                    'flag1': flag1,
+                    'flag2': flag2,
+                    'flag3': flag3,
+                    'flag4': flag4,
+                    'flag5': flag5,
+                    'total': total
+                }
+            }, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({
+                'success': False,
+                'message': e.__str__()
+            }, status=status.HTTP_400_BAD_REQUEST)
