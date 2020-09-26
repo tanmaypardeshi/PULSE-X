ccc16b9a95b388f47bcaee248f40b131711147c3
tanmaypardeshi <tanmaypardeshi@gmail.com>
2020-09-26 14:15:33 +0530

    Added backup

--- frontend/src/globals.js
+++
@@ -1,9 +0,0 @@
-let context = "TEST"; //TEST (for testing) and PROD (for Production)
-
-export const APP_HOST_NAME =
-  context === "PROD"
-    ? ""
-    : window.location.hostname === "localhost"
-    ? "http://localhost:8000/api"
-    : "https://api.pulsex.tk/api";
-
